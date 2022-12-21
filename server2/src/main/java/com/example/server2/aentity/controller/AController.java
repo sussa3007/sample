c9819e4dbccb2d03e3568bb18576eb12bd4c89ef
sussa3007 <sussa1933@gmail.com>
2022-12-21 10:16:06 +0900

    feat: test1
    test1

--- server2/src/main/java/com/example/server2/aentity/controller/AController.java
+++ server2/src/main/java/com/example/server2/aentity/controller/AController.java
@@ -1,4 +1,18 @@
 package com.example.server2.aentity.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AController {
+
+    @GetMapping("/")
+    public ResponseEntity test(
+            // request Dto
+    ) {
+        // Response Dto Stub
+        return ResponseEntity.ok().body("Response Dto");
+    }
 }
